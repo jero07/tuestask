f5280a3e1f5f46f061495d344592fe373fed38d4
jero07 <theres.07@gmail.com>
2023-05-24 21:10:11 -0700

    fix the sidenav backdrop click

--- tuestask-client/src/app/layout/main/main.component.ts
+++ tuestask-client/src/app/layout/main/main.component.ts
@@ -1,26 +1,34 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
-import { Observable, map, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Component, OnInit, Output, ViewChild } from "@angular/core";
+import { Observable, map, tap, shareReplay } from "rxjs";
+import { MatSidenav } from "@angular/material/sidenav";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"],
 })
 export class MainComponent implements OnInit {
+  isMenuOpen = true;
 
-  public isMenuOpen = true;
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  ngOnInit() {
-    this.isMenuOpen = true;  // Open side menu by default
+  toggle() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
+  closesidenav() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  ngOnInit() {
+    this.isMenuOpen = true; // Open side menu by default
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
--- tuestask-client/src/app/shared/components/header/header.component.ts
+++ tuestask-client/src/app/shared/components/header/header.component.ts
@@ -1,18 +1,26 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
-import { Observable, map, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Component, OnInit,Output,EventEmitter } from "@angular/core";
+import { Observable, map, shareReplay } from "rxjs";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  @Output("toggle") toggle: EventEmitter<any> = new EventEmitter();
+  //@Input() IsmenuOpen: boolean = false;
+
+  OnMenuChange() {
+      this.toggle.emit();
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
